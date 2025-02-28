cda0a226c5729919b211d897d3ba4874e1678282
Farhan Ahmad <farhanhubble@gmail.com>
2025-02-28 10:49:00 +0800

    FIX: Input layer channel dim

--- network.py
+++ network.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
         self.fc1 = nn.Linear(64 * 28 * 28, 128)
         self.fc2 = nn.Linear(128, 11)
