e580a0627d86cccb07aa4e33cf2b073c1b95eb5e
Farhan Ahmad <farhanhubble@gmail.com>
2025-02-28 14:33:23 +0800

    Add dense layer 3

--- network.py
+++ network.py
@@ -7,8 +7,9 @@ class CNN(nn.Module):
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
-        self.fc1 = nn.Linear(128 * 28 * 28, 256)
-        self.fc2 = nn.Linear(256, 11)
+        self.fc1 = nn.Linear(128 * 28 * 28, 128)
+        self.fc2 = nn.Linear(128, 1024)
+        self.fc3 = nn.Linear(1024, 11)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
@@ -16,5 +17,6 @@ class CNN(nn.Module):
         x = F.relu(self.conv3(x))
         x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
