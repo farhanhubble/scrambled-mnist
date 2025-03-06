c926645902143027f7dc5ab9c0a2cd3e3deb5b0c
Farhan Ahmad <farhanhubble@gmail.com>
2025-03-06 17:12:16 +0800

    FEAT: Confusion matrix

--- dataloader.py
+++ dataloader.py
@@ -3,6 +3,13 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import json
 
+class2idx = {
+    "0": 0, "1": 1, "2": 2, "3": 3, "4": 4,
+    "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "69": 10
+}
+
+idx2class = {v: k for k, v in class2idx.items()}
+
 def load_hyperparameters():
     with open("hypers.json", "r") as f:
         return json.load(f)
@@ -18,6 +25,11 @@ def get_dataloaders():
     
     train_set = datasets.ImageFolder(root="data/train/augmented", transform=transform)
     test_set = datasets.ImageFolder(root="data/test/converted", transform=transform)
+
+    train_set.class_to_idx = class2idx
+    train_set.classes = list(class2idx.keys())
+    test_set.class_to_idx = class2idx
+    test_set.classes = list(class2idx.keys())
 
     train_loader = DataLoader(train_set, batch_size=hyperparams["batch_size"], shuffle=True)
     test_loader = DataLoader(test_set, batch_size=hyperparams["batch_size"], shuffle=False)
--- test.py
+++ test.py
@@ -1,23 +1,28 @@
-import torch
-from network import CNN
-from config import config
-from dataloader import get_dataloaders
-from tqdm import tqdm
 from datetime import datetime
 import numpy as np
+import numpy as np
+from datetime import datetime
+import torch
+
 import pandas as pd
+from tqdm import tqdm
+
+from config import config
+from dataloader import get_dataloaders, idx2class
+from network import CNN
 
 
 def create_confusion_matrix():
     data = np.zeros((11, 11), dtype=int)
-    columns = [str(i) for i in range(10)] + ["scrambled"]
-    indices = [str(i) for i in range(10)] + ["scrambled"]
+    columns = [str(i) for i in range(10)] + ["69"]
+    indices = [str(i) for i in range(10)] + ["69"]
     return pd.DataFrame(data, columns=columns, index=indices)
 
 def update_confusion_matrix(confusion_matrix, predicted, labels):
+    predicted = predicted.cpu().numpy()
+    labels = labels.cpu().numpy()
     for i in range(len(predicted)):
-        confusion_matrix[labels[i]][predicted[i]] += 1
-
+        confusion_matrix.loc[idx2class[labels[i]], idx2class[predicted[i]]] += 1
 
 
 def evaluate():
@@ -26,7 +31,7 @@ def evaluate():
     model.load_state_dict(torch.load(config.saved_model_dir + "/" + config.model_name))
     model.eval()
 
-    confusion_matrix = np.zeros((11, 11), dtype=int)
+    confusion_matrix = create_confusion_matrix()
 
     correct, total = 0, 0
     with open(config.report_file, "a") as f:
@@ -43,3 +48,5 @@ def evaluate():
     print(f"Accuracy: {test_accuracy:.2f}%")
     with open(config.report_file, "a") as f:
         f.write(f"[{datetime.now()}] Test accuracy: {test_accuracy}\n")
+        f.write(f"[{datetime.now()}] Confusion matrix:\n")
+        confusion_matrix.to_markdown(f, 'a')
