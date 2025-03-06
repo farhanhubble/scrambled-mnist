f1bbc78fa461c04d3f143d7039465b917667d35c
Farhan Ahmad <farhanhubble@gmail.com>
2025-03-06 15:33:57 +0800

    DVC: Record experiment

--- network.py
+++ network.py
@@ -6,12 +6,14 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-        self.fc1 = nn.Linear(64 * 28 * 28, 128)
+        self.fc1 = nn.Linear(64 * 14 * 14, 128)
         self.fc2 = nn.Linear(128, 11)
+        self.pool = nn.MaxPool2d(2, 2)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
+        x = self.pool(x)
         x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
