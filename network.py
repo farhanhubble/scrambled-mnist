5abd3f2fe45828fd06536dbf7a875097c5bbf312
Farhan Ahmad <farhanhubble@gmail.com>
2025-03-06 16:04:02 +0800

    Record results

--- network.py
+++ network.py
@@ -15,8 +15,6 @@ class CNN(nn.Module):
         self.bn3 = nn.BatchNorm2d(128)
 
         self.pool = nn.MaxPool2d(2, 2)  # Reduces spatial dimensions by half
-        self.dropout = nn.Dropout(0.5)
-
         self.fc1 = nn.Linear(128 * 3 * 3, 128)
         self.fc2 = nn.Linear(128, 1024)
         self.fc3 = nn.Linear(1024, 11)  # Output 10 classes
@@ -28,9 +26,7 @@ class CNN(nn.Module):
         
         x = x.view(x.size(0), -1)  # Flatten
         x = F.relu(self.fc1(x))
-        x = self.dropout(x)
         x = F.relu(self.fc2(x))
-        x = self.dropout(x)
         x = self.fc3(x)
         return x
 
