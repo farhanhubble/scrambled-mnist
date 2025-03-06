8b6abb8b524230e44201eb27305da0cbb0ded8c0
Farhan Ahmad <farhanhubble@gmail.com>
2025-03-06 15:48:14 +0800

    DVC: Update experiment results

--- network.py
+++ network.py
@@ -1,20 +1,36 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+import torch.nn as nn
+import torch.nn.functional as F
+
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-        self.fc1 = nn.Linear(64 * 14 * 14, 128)
-        self.fc2 = nn.Linear(128, 11)
-        self.pool = nn.MaxPool2d(2, 2)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+
+        self.pool = nn.MaxPool2d(2, 2)  # Reduces spatial dimensions by half
+        self.dropout = nn.Dropout(0.5)
+
+        self.fc1 = nn.Linear(128 * 3 * 3, 128)
+        self.fc2 = nn.Linear(128, 1024)
+        self.fc3 = nn.Linear(1024, 11)  # Output 10 classes
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = self.pool(x)
-        x = x.view(x.size(0), -1)
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))  # Output: 128x3x3
+        
+        x = x.view(x.size(0), -1)  # Flatten
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
         return x
+
